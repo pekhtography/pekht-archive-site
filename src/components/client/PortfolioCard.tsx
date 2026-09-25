85188972f63fa06db98dcbf215771e68e49832aa
pekhtography <pekhtography@gmail.com>
2026-09-25 13:33:26 +0300

    fix: remove duplicate base-path import

--- src/components/client/PortfolioCard.tsx
+++ src/components/client/PortfolioCard.tsx
@@ -2,7 +2,6 @@
 
 import type { portfolioConfig } from "@/lib/types";
 import { sitePath } from "@/lib/site-url";
-import { sitePath } from "@/lib/site-url";
 
 interface PortfolioCardProps {
   item: portfolioConfig & { images?: string[] };
